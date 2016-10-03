91e99b914a0784a485c780e983e55d9dec4288b3
Surjit <surjit_sen@yahoo.com>
2016-10-02 23:30:52 -0400

    Changed http to https

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 angular.module('NarrowItDownApp', [])
   .controller('NarrowItDownController', NarrowItDownController)
   .service('MenuSearchService', MenuSearchService)
-  .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com/menu_items.json")
+  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json")
   .directive('foundItems', FoundItemsDirective);
 
 function FoundItemsDirective() {
